15d5261902f6b9ccb8d655bd8b661750111d56af
Ismail Mumin <imumin1@student.gsu.edu>
2020-09-04 11:01:07 -0400

    Initial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,33 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		driver = new ChromeDriver();
+	}
+	@Test
+	public void doLogin() throws InterruptedException {
+		driver.get("https://idp.gsu.edu/");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html//input[@id='loginForm:username']")).sendKeys("wow@gmail.com");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html//input[@id='loginForm:password']")).sendKeys("blah");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html//input[@id='loginForm:loginButton']")).click();
+	}
+//	@AfterSuite
+//	public void tearDown() {
+//		driver.quit();
+//	
+//	}
+
+}
